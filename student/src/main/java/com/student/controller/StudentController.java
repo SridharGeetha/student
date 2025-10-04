bdee14cb842863c97b697e10d8d5f938cc8d3036
hemanth <phemanth4647@gmail.com>
2025-10-04 06:29:12 +0000

    changes by hem

--- student/src/main/java/com/student/controller/StudentController.java
+++ student/src/main/java/com/student/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.student.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.dto.Student;
@@ -11,12 +13,17 @@ import com.student.repository.StudentRepository;
 
 @RestController
 public class StudentController {
-    
+
     @Autowired
     StudentRepository repository;
 
     @GetMapping("/getallstudents")
-    public ArrayList<Student> getAll(){
+    public ArrayList<Student> getAll() {
         return (ArrayList<Student>) repository.findAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@RequestParam Long id) {
+        repository.deleteById(id);
     }
 }
